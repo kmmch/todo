4e866819bd60213de9f2116c0bb96828df8a4175
kmmch <kmmch.tech@gmail.com>
2023-11-18 13:46:34 +0900

    npm i --save-dev @types/react-helmet

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,8 +1,10 @@
+import { Helmet } from "react-helmet";
 import { Text } from "@chakra-ui/react"
 
 export default function Todo() {
     return (
         <>
+            <Helmet title="Todo" />
             <Text>Todo</Text>
         </>
     );
